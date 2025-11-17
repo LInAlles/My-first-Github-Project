e56dba4134379a0a420c80656dd580f8d4444097
Halimat <egbeolowohalimat@gmail.com>
2025-11-17 11:51:33 +0100

    Our first Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+GIT
+    Git is a sourc code management platform
+    it is used to track changes in our source code
+
+
+GITHUB
+    A central
+
+KEYWORDS USED IN GIT
+git init: to make a folder to become a git repository
+"""
