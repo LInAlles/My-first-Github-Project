c2548706a5dd24763e01ae8b3203779de1091c17
Halimat <egbeolowohalimat@gmail.com>
2025-11-17 12:44:18 +0100

    fourth commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """
 GIT
-    Git is a sourc code management platform
+    Git is a source code management platform
     it is used to track changes in our source code
 
 
@@ -9,4 +9,12 @@ GITHUB
 
 KEYWORDS USED IN GIT
 git init: to make a folder to become a git repository
+git add . OR git add --all or (git add -A): to add all files at once
+
+COMMIT: taking a screenshot 
+git commit -m "Our first commit"
+git log: to get history of commit made
+git log --oneline: to get a shorter version of commit history
+.gitignore :file created to indicate files not to be tracked by git
+git reset --hard : delete every current changes up till the first commit 
 """
